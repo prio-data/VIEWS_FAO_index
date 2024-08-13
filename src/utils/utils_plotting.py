1440d06a2e4375de74954e0eb84d75e8ccc19afa
Polichinl <simmaa@prio.org>
2024-08-13 15:48:58 +0200

    lastplot

--- src/utils/utils_plotting.py
+++ src/utils/utils_plotting.py
@@ -94,7 +94,7 @@ def plot_time_series(df, country_ids, feature, time_periods=None, figsize=(12, 8
     plt.xlabel('Time Period', fontsize=14)
     plt.ylabel(feature, fontsize=14)
     plt.grid(True, linestyle='--', alpha=0.7)
-    plt.legend(loc='upper left', bbox_to_anchor=(0.7, 1), fontsize=12)
+    plt.legend(loc='upper left', bbox_to_anchor=(0.8, 1), fontsize=12)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
 
@@ -132,7 +132,7 @@ def plot_time_series(df, country_ids, feature, time_periods=None, figsize=(12, 8
 
         # Create OffsetImage and AnnotationBbox
         imagebox = OffsetImage(image, zoom=0.3, alpha=0.7)
-        ab = AnnotationBbox(imagebox, (0.85, 0.85), frameon=False, xycoords='axes fraction', zorder=3)
+        ab = AnnotationBbox(imagebox, (0.9, 0.85), frameon=False, xycoords='axes fraction', zorder=3)
 
     # Add AnnotationBbox to the plot
     plt.gca().add_artist(ab)
@@ -162,7 +162,7 @@ def plot_feature_histograms(df, country_ids, feature, figsize=(16, 8), PATH=PATH
 
     # Determine the number of rows and columns for subplots
     num_countries = len(country_ids)
-    num_cols = min(4, num_countries)
+    num_cols = min(3, num_countries)
     num_rows = (num_countries + num_cols - 1) // num_cols
 
     # check that wither month_id or year_id is in the columns
@@ -231,6 +231,105 @@ def plot_feature_histograms(df, country_ids, feature, figsize=(16, 8), PATH=PATH
     # Adjust layout and show the plot
     plt.tight_layout(rect=[0, 0, 1, 0.95])  # Adjust rect to make space for the super title
     plt.show()
+
+
+def plot_contry_period_map(df, country_id, feature, periods, figsize=(16, 8)):
+
+    # Determine the number of rows and columns for subplots
+    num_periods = len(periods)
+    num_cols = min(3, num_periods)
+    num_rows = (num_periods + num_cols - 1) // num_cols
+
+    # adjust marker size
+    base_marker_size = 64  # Base marker size
+    marker_size = base_marker_size / len(periods)**0.18  # Adjust marker size based on the number of periods
+
+    # check that wither month_id or year_id is in the columns
+    if 'month_id' in df.columns:
+        time_period = 'month_id'
+        time_period_name = 'monthly'
+    
+    elif 'year_id' in df.columns:
+        time_period = 'year_id'
+        time_period_name = 'yearly'
+
+    else:
+        raise ValueError('Time unit not found in the data. Please check the data.')
+    
+    # Set the figure size
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()  # Flatten the axes array for easy iteration
+
+    # Set the style
+    sns.set(style="whitegrid")
+
+    # Plot each country's histogram in a subplot
+    for idx, periode in enumerate(periods):
+        
+        # Filter the data for the specified country and feature
+        sub_df =  df[(df['c_id'] == country_id) & (df[time_period] == periode)][['row', 'col', feature, time_period]]
+
+        # get the min and max values of the feature for that contry over all the periods
+        vmin = df[(df['c_id'] == country_id) & (df[time_period].isin(periods))][feature].min()
+        vmax = df[(df['c_id'] == country_id) & (df[time_period].isin(periods))][feature].max()
+
+
+        # is the data frame empty?
+        if sub_df.empty:
+            print(f"No data found for country {country_id}, {feature} and {time_period} {periode}")
+            continue
+
+        # create the scatter
+            # if the feature name contains "likelihood", set the color map to 'viridis'
+        cmap = 'rainbow_r' if 'likelihood' in feature else 'rainbow'
+
+        # Plot monthly data
+        sc1 = axes[idx].scatter(sub_df[sub_df[time_period] == periode]['col'], 
+                        sub_df[sub_df[time_period] == periode]['row'], 
+                        c=sub_df[sub_df[time_period] == periode][feature], 
+                        cmap=cmap, marker='s', 
+                        s=marker_size, edgecolor=None, linewidth=None, alpha=1, vmin= vmin, vmax= vmax)
+    
+
+        axes[idx].set_title(f'{time_period_name} Data for {periode}', fontsize=16)
+        axes[idx].set_aspect('equal', 'box')
+
+    cbar = fig.colorbar(sc1, ax=axes, orientation='horizontal', pad=0.2, aspect=50)
+    cbar.set_label(feature)
+
+    # Remove any unused subplots
+    for j in range(idx + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    # country name:
+    country_dict = get_country_names_by_ids([country_id])
+    country_name = list(country_dict.values())[0]
+
+    # Add a super title
+    fig.suptitle(f'Map of {feature.replace("_", " ").title()} in country {country_name} (ID: {country_id}), ({time_period_name} data)', fontsize=16, y = 1)
+
+    # Add a logo next to the super title if available
+    PATH_logo = get_logo_path(PATH) / 'VIEWS_logo.png'
+    
+    if not PATH_logo.is_file():
+        print("Logo not found, please make sure the logo is available in the logos folder")
+    else:
+        # Load the image
+        image = plt.imread(PATH_logo)
+
+        # Create OffsetImage and AnnotationBbox
+        imagebox = OffsetImage(image, zoom=0.2, alpha=0.7)
+        ab = AnnotationBbox(imagebox, (0.7, 0.82), frameon=False, xycoords='figure fraction', zorder=3)
+        fig.add_artist(ab)
+
+    # Adjust layout and show the plot
+    #plt.tight_layout(rect=[0, 0, 1, 0.95])  # Adjust rect to make space for the super title
+
+    # Adjust layout to make space for the colorbar
+    fig.subplots_adjust(bottom=0.35, top= 0.9, hspace=0.5, wspace=0.2)
+
+    plt.show()
+
 
 
 def plot_random_monthly_and_yearly_data(df_monthly, df_yearly, feature, year=None, lock_first_month=False, edge_alpha=0.001, cmap_alpha=1, edge_color=None, edge_width=0.4, save_plot=False, title_fontsize=18, label_fontsize=16, tick_fontsize=16):
@@ -326,3 +425,5 @@ def plot_random_monthly_and_yearly_data(df_monthly, df_yearly, feature, year=Non
 
     # Show the plot
     plt.show()
+
+
