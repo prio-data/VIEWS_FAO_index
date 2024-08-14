298b954e6b1112b40fa16e67fa853de95d00d8d8
Polichinl <simmaa@prio.org>
2024-08-14 13:36:43 +0200

    takes series insteaad of df

--- src/utils/utils_p_i.py
+++ src/utils/utils_p_i.py
@@ -2,51 +2,42 @@ import numpy as np
 import pandas as pd
 import unittest
 
-def calculate_p_i_pretest(df, value_col):
+def calculate_p_i_pretest(feature_series):
     """
     Perform pre-tests for the calculate_p_i function.
 
-    This function checks the validity of the input DataFrame and the specified column
-    before proceeding with further calculations. It ensures that the input is a pandas
-    DataFrame, the DataFrame is not empty, the specified column exists, and the column
-    contains numeric values.
+    This function checks the validity of the input Series before proceeding with further calculations.
+    It ensures that the input is a pandas Series, the Series is not empty, and the Series contains numeric values.
 
     Parameters:
-    df (pd.DataFrame): The input DataFrame containing the data.
-    value_col (str): The name of the column in the DataFrame to be checked.
+    feature_series (pd.Series): The input Series containing the data.
 
     Raises:
-    ValueError: If the input is not a pandas DataFrame.
-    ValueError: If the input DataFrame is empty.
-    KeyError: If the specified column does not exist in the DataFrame.
-    TypeError: If the specified column does not contain numeric values.
+    ValueError: If the input is not a pandas Series.
+    ValueError: If the input Series is empty.
+    TypeError: If the Series does not contain numeric values.
     """
     
-    # check that the input is a DataFrame
-    if not isinstance(df, pd.DataFrame):
-        raise ValueError("Input is not a pandas DataFrame")
+    # Check that the input is a Series
+    if not isinstance(feature_series, pd.Series):
+        raise ValueError("Input is not a pandas Series")
 
-    # Check if the DataFrame is empty
-    if df.empty:
-        raise ValueError("The input DataFrame is empty.")
+    # Check if the Series is empty
+    if feature_series.empty:
+        raise ValueError("The input Series is empty.")
     
-    # Check if the column exists in the DataFrame
-    if value_col not in df.columns:
-        raise KeyError(f"The column '{value_col}' does not exist in the DataFrame.")
-    
-    # Check if the column contains numeric values
-    if not np.issubdtype(df[value_col].dtype, np.number):
-        raise TypeError(f"The column '{value_col}' does not contain numeric values.")
+    # Check if the Series contains numeric values
+    if not np.issubdtype(feature_series.dtype, np.number):
+        raise TypeError("The Series does not contain numeric values.")
 
 
 def calculate_p_i_posttest(p_i_values, feature_values):
     """
     Perform post-tests for the calculate_p_i function.
 
-    This function checks the validity of the output p_i values
-    after the calculation. It ensures that the p_i values are a pandas
-    Series, are not empty, do not contain NaN values, are non-decreasing,
-    strictly increasing, within the range [0, 1], start at zero, end at one,
+    This function checks the validity of the output p_i values after the calculation.
+    It ensures that the p_i values are a pandas Series, are not empty, do not contain NaN values,
+    are non-decreasing, strictly increasing, within the range [0, 1], start at zero, end at one,
     are properly normalized, and have the correct length.
 
     Parameters:
@@ -92,12 +83,10 @@ def calculate_p_i_posttest(p_i_values, feature_values):
     
     # Check that the p_i values start at zero
     if not np.isclose(p_i_values.iloc[0], 0, atol=1e-5):
-        print(p_i_values.iloc[-1])
         raise ValueError(f"p_i values do not start at zero. Start value: {p_i_values.iloc[0]}")
 
     # Check that the p_i values end at one
     if not np.isclose(p_i_values.iloc[-1], 1, atol=1e-5):
-        print(p_i_values.iloc[0])
         raise ValueError(f"p_i values do not end at one. End value: {p_i_values.iloc[-1]}")
     
     # Check proper normalization
@@ -109,7 +98,7 @@ def calculate_p_i_posttest(p_i_values, feature_values):
         raise ValueError("The length of the p_i values does not match the number of unique values in the input data.")
 
 
-def calculate_p_i(df, value_col):
+def calculate_p_i(feature_series):
     """
     Calculate the cumulative distribution of values across all voxels.
 
@@ -118,8 +107,7 @@ def calculate_p_i(df, value_col):
     represents 1 - CDF.
 
     Parameters:
-    df (pd.DataFrame): The dataframe containing voxel data.
-    value_col (str): The name of the column containing grid cell values.
+    feature_series (pd.Series): The series containing voxel data.
 
     Returns:
     pd.Series: A series representing the cumulative distribution (p_i), which is the probability
@@ -128,10 +116,10 @@ def calculate_p_i(df, value_col):
     """
 
     # Perform pre-tests
-    calculate_p_i_pretest(df, value_col)
+    calculate_p_i_pretest(feature_series)
     
     # Calculate the frequency of each unique value
-    value_counts = df[value_col].value_counts().sort_index(ascending=False)
+    value_counts = feature_series.value_counts().sort_index(ascending=False)
     
     # Compute the cumulative distribution
     p_i_unnormed = value_counts.cumsum()
@@ -140,6 +128,6 @@ def calculate_p_i(df, value_col):
     p_i = p_i_unnormed / p_i_unnormed.iloc[-1]
 
     # Perform post-tests
-    calculate_p_i_posttest(p_i, df[value_col])
+    calculate_p_i_posttest(p_i, feature_series)
 
     return p_i
