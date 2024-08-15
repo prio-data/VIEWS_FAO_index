26e6a7a1fed3e9bcbb278e88bc5be1359beec82f
Polichinl <simmaa@prio.org>
2024-08-15 10:45:24 +0200

    updated dataloader

--- src/dataloaders/get_full_base.py
+++ src/dataloaders/get_full_base.py
@@ -5,32 +5,18 @@ import pandas as pd
 
 from viewser import Queryset, Column
 
-def get_base_input_data_config():
-    """
-    Contains the configuration for the base input data in the form of a viewser queryset. 
-    This configuration is "behavioral" so modifying it will affect the model's runtime behavior 
-    and integration into the deployment system. There is no guarantee that the model will work 
-    if the input data configuration is changed here without changing the model settings and architecture accordingly.
 
-    It is considered the base input data configuration because it contains the data for state based conflicts, non-state based conflicts, one-sided violence,
-    population data, month_id, year_id, country_id, and the grid row and column.
+import sys
+from pathlib import Path
 
-    Returns:
-    queryset_base (Queryset): A queryset containing the base data for the model training.
-    """
+PATH = Path(__file__)
+sys.path.insert(0, str(Path(*[i for i in PATH.parts[:PATH.parts.index("VIEWS_FAO_index")+1]]) / "src/utils"))  
 
-    queryset_base = (Queryset("FAO_simon_experiments", "priogrid_month")
-        .with_column(Column("sb_best", from_loa = "priogrid_month", from_column = "ged_sb_best_count_nokgi").transform.missing.replace_na())
-        .with_column(Column("ns_best", from_loa = "priogrid_month", from_column = "ged_ns_best_count_nokgi").transform.missing.replace_na())
-        .with_column(Column("os_best", from_loa = "priogrid_month", from_column = "ged_os_best_count_nokgi").transform.missing.replace_na())
-        .with_column(Column("pop_gpw_sum", from_loa="priogrid_year", from_column="pop_gpw_sum").transform.missing.fill().transform.missing.replace_na())
-        .with_column(Column("month", from_loa = "month", from_column = "month"))
-        .with_column(Column("year_id", from_loa = "country_year", from_column = "year_id"))
-        .with_column(Column("c_id", from_loa = "country_year", from_column = "country_id"))
-        .with_column(Column("col", from_loa = "priogrid", from_column = "col"))
-        .with_column(Column("row", from_loa = "priogrid", from_column = "row")))
+from set_paths import setup_project_paths, setup_root_paths, get_logo_path, get_data_paths
+setup_project_paths(PATH)
 
-    return queryset_base
+from config_input_data import get_base_input_data_config
+
 
 def fetch_data_from_viewser():
     """
@@ -230,5 +216,6 @@ def fetch_views_df(PATH_RAW):
     return df
 
 if __name__ == "__main__":
-    PATH_RAW = '/home/simon/Documents/scripts/VIEWS_FAO_index/data/raw_viewser'
-    df = fetch_views_df(PATH_RAW)
+
+    PATH_RAW_VIEWSER, PATH_RAW_EXTERNAL, PATH_PROCESSED, PATH_GENERATED = get_data_paths(PATH)
+    df = fetch_views_df(PATH_RAW_VIEWSER)
