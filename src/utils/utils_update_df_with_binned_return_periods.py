f5607ae7fbc5fb88dc0ad43d1dfb7bb5c791268c
Polichinl <simmaa@prio.org>
2024-08-19 19:58:13 +0200

    just the base

---
+++ src/utils/utils_update_df_with_binned_return_periods.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+import os
+from pathlib import Path
+import sys
+
+import sys
+from pathlib import Path
+
+PATH = Path(__file__)
+sys.path.insert(0, str(Path(*[i for i in PATH.parts[:PATH.parts.index("VIEWS_FAO_index")+1]]) / "src/utils"))  
+
+from set_paths import setup_project_paths
+setup_project_paths(PATH)
