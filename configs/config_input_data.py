52b4f2d3a7d5d774f38e54158d3883753acb49be
Polichinl <simmaa@prio.org>
2024-08-20 10:19:05 +0200

    now with correct queryset

--- configs/config_input_data.py
+++ configs/config_input_data.py
@@ -20,9 +20,9 @@ def get_base_input_data_config():
     """
 
     queryset_base = (Queryset("FAO_simon_experiments", "priogrid_month")
-        .with_column(Column("sb_best", from_loa = "priogrid_month", from_column = "ged_sb_best_count_nokgi").transform.missing.replace_na())
-        .with_column(Column("ns_best", from_loa = "priogrid_month", from_column = "ged_ns_best_count_nokgi").transform.missing.replace_na())
-        .with_column(Column("os_best", from_loa = "priogrid_month", from_column = "ged_os_best_count_nokgi").transform.missing.replace_na())
+        .with_column(Column("sb_best", from_loa = "priogrid_month", from_column = "ged_sb_best_sum_nokgi").transform.missing.replace_na())
+        .with_column(Column("ns_best", from_loa = "priogrid_month", from_column = "ged_ns_best_sum_nokgi").transform.missing.replace_na())
+        .with_column(Column("os_best", from_loa = "priogrid_month", from_column = "ged_os_best_sum_nokgi").transform.missing.replace_na())
         .with_column(Column("pop_gpw_sum", from_loa="priogrid_year", from_column="pop_gpw_sum").transform.missing.fill().transform.missing.replace_na())
         .with_column(Column("month", from_loa = "month", from_column = "month"))
         .with_column(Column("year_id", from_loa = "country_year", from_column = "year_id"))
@@ -31,3 +31,4 @@ def get_base_input_data_config():
         .with_column(Column("row", from_loa = "priogrid", from_column = "row")))
 
     return queryset_base
+
